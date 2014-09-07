b52b3d1f768417050aef1fdf2f3e3713373cd2a3
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2014-09-07 15:12:14 +0200

    adding method to handle file upload

--- webapp/src/main/java/pl/jmilkiewicz/fas/web/controller/DocumentsController.java
+++ webapp/src/main/java/pl/jmilkiewicz/fas/web/controller/DocumentsController.java
@@ -1,23 +1,32 @@
 package pl.jmilkiewicz.fas.web.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pl.jmilkiewicz.fas.application.ApplicationController;
+import pl.jmilkiewicz.fas.application.FileMetaData;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 @Controller
 @RequestMapping("/documents")
 public class DocumentsController {
+    @Autowired
+    private ApplicationController applicationController;
 
-
-
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<?> displayPage() throws IOException {
-        return new ResponseEntity<>(HttpStatus.OK);
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> getAllDocuments(@RequestParam("file") MultipartFile file) throws IOException, ParseException {
+        SpringNavigator springNavigator = new SpringNavigator(ServletUriComponentsBuilder.fromCurrentServletMapping());
+        applicationController.addFile(file.getInputStream(), new FileMetaData("fake","2000-01-01","fakeFileName"), springNavigator);
+        return springNavigator.getResult();
     }
 
 }
