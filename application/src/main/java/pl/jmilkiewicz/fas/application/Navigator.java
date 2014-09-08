1b31adb6d5f7e0c1caad4d14642ca77f4c5c382d
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2014-09-08 16:45:22 +0200

    removing some auto generated shit

--- application/src/main/java/pl/jmilkiewicz/fas/application/Navigator.java
+++ application/src/main/java/pl/jmilkiewicz/fas/application/Navigator.java
@@ -1,12 +1,5 @@
 package pl.jmilkiewicz.fas.application;
 
-/**
- * Created with IntelliJ IDEA.
- * User: milus
- * Date: 9/5/14
- * Time: 12:49 PM
- * To change this template use File | Settings | File Templates.
- */
 public interface Navigator {
     void goTo(ViewReference viewReference);
     void display(View view);
