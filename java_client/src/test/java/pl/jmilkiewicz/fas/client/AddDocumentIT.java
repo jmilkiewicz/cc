f2cad6b2851f40e4296a18cf035ede8a3d1d9aee
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2014-09-18 20:39:31 +0200

    using HttpClient as HttpConnection backend

--- java_client/src/test/java/pl/jmilkiewicz/fas/client/AddDocumentIT.java
+++ java_client/src/test/java/pl/jmilkiewicz/fas/client/AddDocumentIT.java
@@ -1,20 +1,25 @@
 package pl.jmilkiewicz.fas.client;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import pl.jmilkiewicz.fas.client.support.NotFollowRedirectionStrategy;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 
 public class AddDocumentIT {
     @Rule
@@ -29,18 +34,28 @@ public class AddDocumentIT {
         writer.flush();
         writer.close();
 
-        RestTemplate restTemplate = new RestTemplate();
+        CloseableHttpClient httpClient = HttpClientBuilder.create().setRedirectStrategy(new NotFollowRedirectionStrategy()).build();
+
+        RestTemplate restTemplate = new RestTemplate(new InterceptingClientHttpRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient), Arrays.<ClientHttpRequestInterceptor>asList(new ClientHttpRequestInterceptor(){
+            @Override
+            public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+                request.getHeaders().add("Authorization", getAuthorizationValue());
+                ClientHttpResponse execute = execution.execute(request, body);
+                return  execute;
+            };
+        })));
 
         MultiValueMap<String, Object> parts = new
                 LinkedMultiValueMap<String, Object>();
-        //parts.add("field 1", "value 1");
+        parts.add("fileName", file.getName());
+        parts.add("documentDate", "2011-02-02");
         parts.add("file", new
                 FileSystemResource(file));
-        MultiValueMap headers = new LinkedMultiValueMap();
-        headers.add("Authorization", getAuthorizationValue());
+        //MultiValueMap headers = new LinkedMultiValueMap();
+        //headers.add();
 
 
-        URI uri = restTemplate.postForLocation("http://localhost:8080/documents", new HttpEntity<>(parts, headers));
+        URI uri = restTemplate.postForLocation("http://localhost:9090/documents", parts);
 
         System.out.println(uri);
     }
