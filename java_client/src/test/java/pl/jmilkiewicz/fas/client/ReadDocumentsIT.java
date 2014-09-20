e631c1315c6303bbed5c08b319db1f84da01ec42
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2014-09-20 17:02:34 +0200

    assertions for getting docsUploadedByX are now more robust. Configuring objectMapper to use dateFormat and BigInteger for decimal JSON values

--- java_client/src/test/java/pl/jmilkiewicz/fas/client/ReadDocumentsIT.java
+++ java_client/src/test/java/pl/jmilkiewicz/fas/client/ReadDocumentsIT.java
@@ -1,20 +1,34 @@
 package pl.jmilkiewicz.fas.client;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.ResourceHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import pl.jmilkiewicz.fas.client.support.NotFollowRedirectionStrategy;
 
-import java.io.File;
+import javax.xml.transform.Source;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.math.BigInteger;
+import java.util.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -28,26 +42,51 @@ public class ReadDocumentsIT {
 
     @Before
     public void setUp(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new ISO8601DateFormat());
+        objectMapper.enable(DeserializationFeature.USE_BIG_INTEGER_FOR_INTS);
+
         CloseableHttpClient httpClient = HttpClientBuilder.create().setRedirectStrategy(new NotFollowRedirectionStrategy()).build();
         RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+
+        restTemplate.setMessageConverters(Arrays.<HttpMessageConverter<?>>asList(
+                                                    new ByteArrayHttpMessageConverter(),
+                                                    new StringHttpMessageConverter(),
+                                                    new ResourceHttpMessageConverter(),
+                                                    new SourceHttpMessageConverter<Source>(),
+                                                    new AllEncompassingFormHttpMessageConverter(),
+                                                    mappingJackson2HttpMessageConverter));
+
         fasClient = new FasClient(restTemplate, credentials);
     }
 
     @Test
     public void shallReturnDocumentsOfParticularUser() throws IOException {
-        File file = folder.newFile("testFile.txt");
-        fasClient.addFile(file,"2011-02-02");
+        String uniqueFileName = "ABC" + System.currentTimeMillis();
+        fasClient.addFile(folder.newFile(uniqueFileName),"2011-02-02");
 
         Map<String, Object> documents = fasClient.getDocumentsOf(credentials.getName());
-        assertContainsAtLeastOneDocument(documents);
+        assertContainsDocumentOf(documents, credentials.getName());
     }
 
-    private void assertContainsAtLeastOneDocument(Map<String,Object> documents) {
-        assertThat(documents, notNullValue());
-        assertThat(documents, Matchers.<String>hasKey("data"));
-        List<?> documentEntries = (List<?>) documents.get("data");
+    private void assertContainsDocumentOf(Map<String, Object> response, String name) {
+        assertDataNotEmpty(response);
+        List<Map<String,Object>> documentEntries = (List<Map<String,Object>>) response.get("data");
         assertThat("at least a single document must be returned", documentEntries, hasSize(greaterThan(0)));
+
+        for (Map<? extends  String, ? extends  Object> documentEntry : documentEntries) {
+            assertThat("invalid upload Person", documentEntry, Matchers.<String, Object>hasEntry("uploadPerson", name));
+        }
     }
+
+
+    private void assertDataNotEmpty(Map<String, Object> response) {
+        assertThat(response, notNullValue());
+        assertThat(response, Matchers.<String>hasKey("data"));
+    }
+
 
 
 }
